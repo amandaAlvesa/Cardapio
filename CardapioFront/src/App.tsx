228ba5ac185d064fb2ca4eb97cfe4e800fa69b23
Amanda Alves <amandaalves51030@gmail.com>
2023-08-30 16:49:21 -0300

    ADD PRODUTO NA PAGE

--- CardapioFront/src/App.tsx
+++ CardapioFront/src/App.tsx
@@ -3,9 +3,16 @@ import './App.css'
 import { Card } from './components/card/card';
 import { FoodData } from './interface/FoodData';
 import { useFoodData } from './hooks/useFoodData';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
   const {data} = useFoodData();
+  const [isModalOpen, setIsModalOpen] =useState(false); 
+
+  const handleOpenModal= () => {
+    setIsModalOpen (prev => !prev)
+  }
+
   return (
       <div className='container'>
       <h1>Cardapio</h1>
@@ -18,6 +25,8 @@ function App() {
       />
       )}
     </div>
+      {isModalOpen && <CreateModal closeModal={handleOpenModal  }/>}
+      <button onClick={handleOpenModal}>novo</button>
     </div>
   )
 }
